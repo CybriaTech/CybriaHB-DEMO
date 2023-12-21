71c3d38e2819c5f57d1660c7ca8c7b83cd57a58a
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-12-20 21:58:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-fetch("https://demo-api.tutturu.workers.dev")
-  .then(response => response.json())
-  .then(data => {
-    window.location.replace(data.url);
-  })
-  .catch(error => console.log(error));
+function fetchDataAndRedirect() {
+  fetch("https://demo-api.tutturu.workers.dev")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      window.location.replace(data.url);
+    })
+    .catch(error => {
+      console.error('Error fetching and redirecting:', error);
+    });
+}
+
+function fetchDataAndLogURL() {
+  fetch("https://demo-api.tutturu.workers.dev")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('URL from the API:', data.url);
+    })
+    .catch(error => {
+      console.error('Error fetching and logging URL:', error);
+    });
+}
+
+fetchDataAndRedirect();
+fetchDataAndLogURL();
